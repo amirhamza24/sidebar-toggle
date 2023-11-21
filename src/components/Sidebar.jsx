47748a1e99625bf78c4e592e260f212df39e5e55
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-22 01:30:18 +0600

    some error solve

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -49,11 +49,13 @@ const Sidebar = ({children}) => {
     return (
         <div className="container">
            <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
-               <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
-                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+               <div className="top_section flex items-center">
+                   <div style={{marginLeft: isOpen ? "0px" : "0px"}} className="bars">
                        <FaBars onClick={toggle}/>
                    </div>
+                   {/* <div className='flex justify-end'>
+                        <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                   </div> */}
                </div>
                {
                    menuItem.map((item, index)=>(
