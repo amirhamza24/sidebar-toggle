658da97e02cfb04fd4b7f129f3d4ba663f111efe
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-30 05:38:33 +0600

    pagination update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,6 +14,14 @@ import { NavLink } from 'react-router-dom';
 const Sidebar = ({children}) => {
     const[isOpen ,setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
+    // let [pageCount,setPage]=useState(1);
+    // let [page,setPg]=useState(0);
+    // const limit = 10;
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [attReportList, setAttReportList] = useState([]);
+    // const [attReportList2, setAttReportList2] = useState([]);
+    // const [totalPage,setTotalPage]=useState(null);
+
     const menuItem=[
         {
             path:"/",
@@ -46,6 +54,50 @@ const Sidebar = ({children}) => {
             icon:<FaThList/>
         }
     ]
+
+    // const next=async()=>{
+    //     // setPage((page)=>page+10);
+    //     const newPage = page + limit;
+    //     setPg(newPage);
+    //     console.log(`page here ${newPage}`);
+    //     setPage((pre)=>pre+1);
+      
+    //     setIsLoading(true);
+    //     setAttReportList([]);
+    //     setAttReportList2([]);
+      
+    //     // submit(newPage);
+    //     setIsLoading(false);
+        
+    //     console.log('clicked');
+    // }
+    
+    // const previous=async()=>{
+    //     // setPage((pre)=>pre-10);
+    //     const newPage = page - limit;
+    //     setPg(newPage);
+    //     console.log(`page here ${newPage}`);
+    //     setPage((pre)=>pre-1);
+        
+    //     console.log('clicked');
+    //     setIsLoading(true);
+    //     setAttReportList([]);
+    //     setAttReportList2([]);
+        
+    //     // submit(newPage);
+    //     setIsLoading(false);
+    //     // console.log(result.ATTENDANCE_DETAILS);
+    // }
+
+    // const divideAndCeil = (number) => {
+    //     if (typeof number !== 'number') {
+    //       throw new Error('Input must be a number');
+    //     }
+      
+    //     const re= Math.ceil(number / limit);
+    //     setTotalPage(re);
+    // }
+
     return (
         <div className="container">
            <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
@@ -66,7 +118,29 @@ const Sidebar = ({children}) => {
                    ))
                }
            </div>
-           <main>{children}</main>
+
+            
+            {/* pagination 
+           <div className=' flex flex-row space-x-4 justify-center items-end mt-4 mb-3 lg:w-full md:w-3/4 sm:w-96 pr-6'>
+                <button  disabled={ page===0 ?true:false} onClick={()=>{previous()}}>
+                    <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6  font-bold  ${page ===0 ?'text-white':`text-gray-500`} font-bold`}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                    </svg>
+                </button>
+                
+                <p className={`text-gray-500 text-[18px] font-bold`}>
+                    {pageCount} Of {totalPage}
+                </p> 
+
+
+                <button disabled={attReportList.length ===0 || pageCount===totalPage? true:false} onClick={()=>{next()}}>
+                    <svg   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6  font-bold  ${attReportList.length ===0 || pageCount===totalPage?'text-white':`text-gray-500`} font-bold`}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+            </div> */}
+
+            <main>{children}</main>
         </div>
     );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dashboard = () => {
+
+    let [pageCount,setPage]=useState(1);
+    let [page,setPg]=useState(0);
+    const limit = 10;
+    const [isLoading, setIsLoading] = useState(false);
+    const [attReportList, setAttReportList] = useState([]);
+    const [attReportList2, setAttReportList2] = useState([]);
+    const [totalPage,setTotalPage]=useState(null);
+
+    const next=async()=>{
+        // setPage((page)=>page+10);
+        const newPage = page + limit;
+        setPg(newPage);
+        console.log(`page here ${newPage}`);
+        setPage((pre)=>pre+1);
+      
+        setIsLoading(true);
+        setAttReportList([]);
+        setAttReportList2([]);
+      
+        // submit(newPage);
+        setIsLoading(false);
+        
+        console.log('clicked');
+    }
+    
+    const previous=async()=>{
+        // setPage((pre)=>pre-10);
+        const newPage = page - limit;
+        setPg(newPage);
+        console.log(`page here ${newPage}`);
+        setPage((pre)=>pre-1);
+        
+        console.log('clicked');
+        setIsLoading(true);
+        setAttReportList([]);
+        setAttReportList2([]);
+        
+        // submit(newPage);
+        setIsLoading(false);
+        // console.log(result.ATTENDANCE_DETAILS);
+    }
+
+    const divideAndCeil = (number) => {
+        if (typeof number !== 'number') {
+          throw new Error('Input must be a number');
+        }
+      
+        const re= Math.ceil(number / limit);
+        setTotalPage(re);
+    }
+
     return (
         <div>
             <h1>dashboard page</h1>
+
+            {/*pagination  */}
+           <div className=' flex flex-row space-x-4 justify-center items-end mt-4 mb-3 lg:w-full md:w-3/4 sm:w-96 pr-6'>
+                <button onClick={() => {previous()}}>
+                    <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 font-bold ${page ===0 ?'text-black':`text-gray-500`} font-bold`}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                    </svg>
+                </button>
+                
+                <p className={`text-gray-500 text-[18px] font-bold`}>
+                    {pageCount} Of {totalPage}
+                </p> 
+
+
+                <button disabled={attReportList.length === 0 || pageCount === totalPage? true:false} onClick={()=>{next()}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 font-bold ${attReportList.length === 0 || pageCount===totalPage?'text-white':`text-gray-500`} font-bold`}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+            </div>
         </div>
     );
 };
